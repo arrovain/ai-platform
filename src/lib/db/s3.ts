b80652e0d5ba64ce83adcb6ff38cbe76ef6a78c8
arrovain <arrovain@gmail.com>
2024-05-03 16:28:43 +0300

    embeddings

--- src/lib/db/s3.ts
+++ src/lib/db/s3.ts
@@ -1,7 +1,9 @@
-import { S3 } from "@aws-sdk/client-s3";
-import fs from "fs";
-export async function downloadFromS3(file_key: string): Promise<string> {
-  return new Promise(async (resolve, reject) => {
+import { PutObjectCommandOutput, S3 } from "@aws-sdk/client-s3";
+
+export async function uploadToS3(
+  file: File
+): Promise<{ file_key: string; file_name: string }> {
+  return new Promise((resolve, reject) => {
     try {
       const s3 = new S3({
         region: "ap-southeast-1",
@@ -10,26 +12,31 @@ export async function downloadFromS3(file_key: string): Promise<string> {
           secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!,
         },
       });
+
+      const file_key =
+        "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
+
       const params = {
         Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
         Key: file_key,
+        Body: file,
       };
-
-      const obj = await s3.getObject(params);
-      const file_name = `/tmp/elliott${Date.now().toString()}.pdf`;
-
-      if (obj.Body instanceof require("stream").Readable) {
-        const file = fs.createWriteStream(file_name);
-        file.on("open", function (fd) {
-          obj.Body?.pipe(file).on("finish", () => {
-            return resolve(file_name);
+      s3.putObject(
+        params,
+        (err: any, data: PutObjectCommandOutput | undefined) => {
+          return resolve({
+            file_key,
+            file_name: file.name,
           });
-        });
-      }
+        }
+      );
     } catch (error) {
-      console.error(error);
       reject(error);
-      return null;
     }
   });
+}
+
+export function getS3Url(file_key: string) {
+  const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.ap-southeast-1.amazonaws.com/${file_key}`;
+  return url;
 }
