dc485ef09d5c09f499a2447a49a962e0cc75bdaf
arrovain <arrovain@gmail.com>
2024-05-04 16:33:55 +0300

    button, input and some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { ArrowRight, LogIn } from "lucide-react";
+import FileUpload from "@/components/FileUpload";
+import { checkSubscription } from "@/lib/subscription";
+import SubscriptionButton from "@/components/SubscriptionButton";
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
 
-export default function Home() {
+export default async function Home() {
   const { userId } = await auth();
   const isAuth = !!userId;
   const isPro = await checkSubscription();
